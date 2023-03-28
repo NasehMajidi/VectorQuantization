a4491db85919fd1f59687456abc32cf7cfcf7c89
NasehMajidi <128865850+NasehMajidi@users.noreply.github.com>
2023-03-28 15:18:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+
+
+###############################################################################
+################################### Imports ###################################
+###############################################################################
+
 import cv2
 import numpy as np
 import os
@@ -6,6 +12,11 @@ import warnings
 from LGB import *
 warnings.filterwarnings("ignore")
 
+###############################################################################
+#################################### Main #####################################
+###############################################################################
+
+# Load data
 data_dir = "data1/"# the folder path
 os.listdir(data_dir)
 train_dir = f'{data_dir}Train'
@@ -13,13 +24,15 @@ test_dir = f'{data_dir}Test'
 train_frames = glob.glob(f'{train_dir}/*.jpg')
 test_frames = glob.glob(f'{test_dir}/*.jpg')
 
+# Train-test split
 train_images=np.zeros((256,256,len(train_frames)))
 test_images=np.zeros((256,256,len(test_frames)))
 for i in range(len(train_frames)):
     train_images[:,:,i] = cv2.imread(train_frames[i],cv2.IMREAD_GRAYSCALE)/255
 for i in range(len(test_frames)):
     test_images[:,:,i] = cv2.imread(test_frames[i],cv2.IMREAD_GRAYSCALE)/255
-    
+
+# Running LGB algorithm
 lgb_algorithm = LGB(train_images, test_images,(2,2), 1e-4,2)
 lgb_algorithm.train()
 lgb_algorithm.test()
